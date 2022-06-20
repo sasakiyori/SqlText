08c1a3a32c067b7bbc0b24392a58fe3f92bef332
sasakiyori <sasakiyori33@gmail.com>
2022-06-20 18:21:53 +0800

    feat: add CommandType and ReadonlyCommand implements for postgresql

--- postgres_test.go
+++ postgres_test.go
@@ -57,7 +57,52 @@ select * from test_a;`,
 		t.Run(tc.name, func(t *testing.T) {
 			res := pg.FormatText(pg.RemoveComments(tc.input))
 			if res != tc.output {
-				t.Errorf("expect: %s, actual: %s", tc.output, res)
+				t.Errorf("FormatText expect: %s, actual: %s", tc.output, res)
+			}
+		})
+	}
+}
+
+func TestPostgresqlCommandType(t *testing.T) {
+	pg := PostgresqlText{}
+	testCases := []struct {
+		name     string
+		sql      string
+		cmdType  CmdType
+		readonly bool
+	}{
+		{"do", "DO $$DECLARE r record;$$;", DO, false},
+		{"lock", "lock table films in share mode;", LOCK, false},
+		{"call", "call dosth();", CALL, false},
+		{"copy", "copy test_a from '/path/to/file';", COPY, false},
+		{"drop", "drop table test_a;", DROP, false},
+		{"rule", "create rule rule_a as on update to test_a do insert into test_a values(1,'x');", RULE, false},
+		{"view", "create view vista as select 'hello world';", VIEW, false},
+		{"alter", "alter table test_a add column new_col varchar(30);", ALTER, false},
+		{"fetch", "fetch forward 5 from liahona;", FETCH, true},
+		{"index", "create unique index title_idx on films (title);", INDEX, false},
+		{"grant", "grant insert on films to public;", GRANT, false},
+		{"create", "create table test_a (id int, name text);", CREATE, false},
+		{"select", "select * from test_a;", SELECT, true},
+		{"update", "update test_a set name='x' where id=1;", UPDATE, false},
+		{"insert", "insert into test_a values (1, 'a')", INSERT, false},
+		{"delete", "delete from test_a where id=1;", DELETE, false},
+		{"cursor", "declare liahona scroll cursor for select * from films;", CURSOR, false},
+		{"explain", "explain select * from test_a;", EXPLAIN, true},
+		{"prepare", "prepare foo (int,text) as insert into test_a values($1,$2);", PREPARE, false},
+		{"execute", "execute foo(1,'a');", EXECUTE, false},
+		{"truncate", "truncate table test_a;", TRUNCATE, false},
+		{"checkpoint", "checkpoint;", CHECKPOINT, true},
+		{"transaction", "begin;", TRANSACTION, false},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if ct, err := pg.CommandType(tc.sql); err != nil || ct != tc.cmdType {
+				t.Errorf("CommandType expect: %d, actual: %d, error: %v", tc.cmdType, ct, err)
+			}
+			if ro := pg.ReadonlyCommand(tc.sql); ro != tc.readonly {
+				t.Errorf("ReadonlyCommand expect: %t, actual: %t", tc.readonly, ro)
 			}
 		})
 	}
--- sqltext.go
+++ sqltext.go
@@ -6,4 +6,10 @@ type SqlText interface {
 
 	// FormatText Minimize the amount of white space in the text and keep only one line
 	FormatText(sql string) string
+
+	// CommandType command type of sql text
+	CommandType(sql string) (CmdType, error)
+
+	// ReadonlyCommand check out if the sql is readonly command
+	ReadonlyCommand(sql string) bool
 }
